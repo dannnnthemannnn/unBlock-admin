47836789c2d1a58f77c4e90a3373d734b80739db
Danny Franklin <dannyfranklin@Dannys-Air.fios-router.home>
2017-09-12 19:51:13 -0400

    Attempting to get login working

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -12,6 +12,7 @@ export class LoginComponent {
     username = '';
     password = '';
     error = '';
+    test = 'yellow';
 
     constructor(//private authService: AuthService, 
         private router: Router) {
